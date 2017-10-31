688157eef68d41b0016f70c903dea6fb31a4d281
Casey Marshall <git@cmars.tech>
2017-10-30 22:03:41 -0500

    Colors.

--- main.go
+++ main.go
@@ -4,6 +4,8 @@ import (
 	"image"
 	"image/color"
 	"image/png"
+	"log"
+	"math"
 	"math/rand"
 	"os"
 	"time"
@@ -11,6 +13,7 @@ import (
 
 func main() {
 	options := DefaultOptions()
+	options.Colored = true
 	g := NewGenerator(Spaceship(), options)
 	m := g.Sprite()
 	f, err := os.Create("image.png")
@@ -121,9 +124,9 @@ func (m *Mask) chooseEdges() {
 
 type Options struct {
 	Colored         bool
-	Edgep_ightness  float64
+	EdgeBrightness  float64
 	ColorVariations float64
-	p_ightnessNoise float64
+	BrightnessNoise float64
 	Saturation      float64
 	Random          *rand.Rand
 }
@@ -131,9 +134,9 @@ type Options struct {
 func DefaultOptions() *Options {
 	return &Options{
 		Colored:         false,
-		Edgep_ightness:  0.3,
+		EdgeBrightness:  0.3,
 		ColorVariations: 0.2,
-		p_ightnessNoise: 0.3,
+		BrightnessNoise: 0.3,
 		Saturation:      0.5,
 		Random:          rand.New(rand.NewSource(time.Now().UnixNano())),
 	}
@@ -183,14 +186,123 @@ func (g *Generator) Sprite() *image.RGBA {
 	t.chooseBody(g.options.Random)
 	t.chooseEdges()
 	m := image.NewRGBA(image.Rect(0, 0, g.mask.ImageHeight(), g.mask.ImageWidth()))
-	for y := 0; y < g.mask.ImageHeight(); y++ {
-		for x := 0; x < g.mask.ImageWidth(); x++ {
-			if g.mask.get(x, y) == PixelBorder {
-				m.Set(x, y, color.RGBA{0, 0, 0, 255})
-			} else {
-				m.Set(x, y, color.RGBA{255, 255, 255, 255})
+	/*
+		for y := 0; y < g.mask.ImageHeight(); y++ {
+			for x := 0; x < g.mask.ImageWidth(); x++ {
+				if g.mask.get(x, y) == PixelBorder {
+					m.Set(x, y, color.RGBA{0, 0, 0, 255})
+				} else {
+					m.Set(x, y, color.RGBA{255, 255, 255, 255})
+				}
 			}
+		}
+	*/
+
+	var (
+		borderColor        color.RGBA
+		isVerticalGradient bool
+		ulen, vlen         int
+		hue                float64 = g.options.Random.Float64()
+		saturation         float64 = math.Max(
+			math.Min(g.options.Random.Float64()*g.options.Saturation, 1.0),
+			0)
+	)
+	if g.options.Colored {
+		edgeColor := uint8(255.0 * g.options.EdgeBrightness)
+		borderColor.R = edgeColor
+		borderColor.G = edgeColor
+		borderColor.B = edgeColor
+		borderColor.A = 255
+	} else {
+		borderColor = color.RGBA{0, 0, 0, 255}
+	}
+	if g.options.Random.Float64() > 0.5 {
+		isVerticalGradient = true
+		ulen = g.mask.ImageHeight()
+		vlen = g.mask.ImageWidth()
+	} else {
+		ulen = g.mask.ImageWidth()
+		vlen = g.mask.ImageHeight()
+	}
+
+	for u := 0; u < ulen; u++ {
+		isNewColor := math.Abs(((g.options.Random.Float64()*2.0 - 1.0) +
+			(g.options.Random.Float64()*2.0 - 1.0) +
+			(g.options.Random.Float64()*2.0 - 1.0)) / 3.0)
+		if isNewColor > (1.0 - g.options.ColorVariations) {
+			hue = g.options.Random.Float64()
+		}
+
+		for v := 0; v < vlen; v++ {
+			var (
+				pixel Pixel
+				x, y  int
+				c     color.RGBA
+			)
+			if isVerticalGradient {
+				y, x = u, v
+			} else {
+				x, y = u, v
+			}
+			pixel = t.get(x, y)
+
+			if g.options.Colored {
+				if pixel == PixelBorder {
+					c = borderColor
+				} else if pixel == PixelBody {
+					brightness := math.Sin(
+						(float64(u)/float64(ulen))*math.Pi) * (1.0 - g.options.BrightnessNoise)
+					c = hsl2rgb(hue, saturation, brightness)
+				}
+			} else if pixel == PixelBorder {
+				c = borderColor
+			}
+			log.Println(c)
+
+			m.Set(x, y, c)
 		}
 	}
 	return m
+}
+
+func hsl2rgb(h, s, l float64) color.RGBA {
+	var (
+		i          int
+		f, p, q, t float64
+	)
+	i = int(math.Floor(h * 6.0))
+	f = h*6.0 - float64(i)
+	p = l * (1.0 - s)
+	q = l * (1.0 - f*s)
+	t = l * (1.0 - (1.0-f)*s)
+
+	var c color.RGBA
+	c.A = 255
+	switch i % 6 {
+	case 0:
+		c.R = uint8(l * 255.0)
+		c.G = uint8(t * 255.0)
+		c.B = uint8(p * 255.0)
+	case 1:
+		c.R = uint8(q * 255.0)
+		c.G = uint8(l * 255.0)
+		c.B = uint8(p * 255.0)
+	case 2:
+		c.R = uint8(p * 255.0)
+		c.G = uint8(l * 255.0)
+		c.B = uint8(t * 255.0)
+	case 3:
+		c.R = uint8(p * 255.0)
+		c.G = uint8(q * 255.0)
+		c.B = uint8(l * 255.0)
+	case 4:
+		c.R = uint8(t * 255.0)
+		c.G = uint8(p * 255.0)
+		c.B = uint8(l * 255.0)
+	case 5:
+		c.R = uint8(l * 255.0)
+		c.G = uint8(p * 255.0)
+		c.B = uint8(q * 255.0)
+	}
+	return c
 }
