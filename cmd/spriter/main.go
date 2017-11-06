4d42bfbee5b8fb1a36b18a59c0c5bc0b7ccd324f
Casey Marshall <git@cmars.tech>
2017-11-05 22:24:57 -0600

    Resize images.

--- cmd/spriter/main.go
+++ cmd/spriter/main.go
@@ -5,12 +5,16 @@ import (
 	"log"
 	"os"
 
+	"github.com/nfnt/resize"
+
 	"github.com/cmars/spriter"
 )
 
 func main() {
 	options := spriter.DefaultOptions()
 	options.Colored = true
+	options.ColorVariations = 0.5
+	options.Saturation = 0.8
 	mask := spriter.Spaceship()
 	log.Printf("mask can represent %d bits", mask.BitLen())
 	g := spriter.NewGenerator(mask, options)
@@ -20,7 +24,8 @@ func main() {
 		panic(err)
 	}
 	defer f.Close()
-	err = png.Encode(f, m)
+	resized := resize.Resize(128, 128, m, resize.NearestNeighbor)
+	err = png.Encode(f, resized)
 	if err != nil {
 		panic(err)
 	}
